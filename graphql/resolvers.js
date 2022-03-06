40963ee7240055b162749bc86e6b22290585eaea
Chris Mumma <cmumma.photo@gmail.com>
2022-03-06 15:05:45 -0500

    MongoFix, and initial steps for  user creation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,13 @@
 
+const user = require("../models/user");
+
 module.exports = {
    
     getStatus: async function ({}, req) {
         return {status: 'status got'};
+    },
+
+    createUser: async function({userInput}, req) {
+        return {name: 'status got'};
     }
 }
